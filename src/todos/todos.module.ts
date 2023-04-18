6d931dd6c897508d8c2c1d42c3e3ff8807ada9a5
Morning <morning@class3-3s-iMac.local>
2023-04-18 10:34:01 +0800

    init

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { TodosController } from './todos.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { todoListSchema } from './entities/todo.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Todo',
+        schema: todoListSchema,
+      },
+    ]),
+  ],
   controllers: [TodosController],
-  providers: [TodosService]
+  providers: [TodosService],
 })
 export class TodosModule {}
