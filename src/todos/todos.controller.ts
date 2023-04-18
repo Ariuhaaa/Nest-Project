0c6706f4b49f54e698dbb388d6f8bd8a51f3c9da
Oyunaa <oyuka579@gmail.com>
2023-04-18 10:14:27 +0800

    init

---
+++ src/todos/todos.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Res,
+  HttpStatus,
+} from '@nestjs/common';
+import { TodosService } from './todos.service';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Response } from 'express';
+
+@Controller('todos')
+export class TodosController {
+  constructor(private readonly todosService: TodosService) {}
+
+  @Post()
+  create(@Res() res: Response, @Body() createTodoDto: CreateTodoDto) {
+    // res.json({})
+    // res.status(HttpStatus.I_AM_A_TEAPOT);
+    return this.todosService.create(createTodoDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.todosService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.todosService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
+    return this.todosService.update(+id, updateTodoDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.todosService.remove(+id);
+  }
+}
